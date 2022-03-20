e6b9f3e9685ce00b27b624c32b7398eee91b26b6
Jacqueline Hodgson <jacqueline.e.hodgson@gmail.com>
2022-03-20 17:25:48 -0400

    debug routes

--- routes/api/tagRoutes.js
+++ routes/api/tagRoutes.js
@@ -32,7 +32,7 @@ router.get('/:id', async (req, res) => {
 });
 
 // POST Route to create a new tag;
-router.post('/:id', async (req, res) => {
+router.post('/', async (req, res) => {
   try {
     const data = await Tag.create(req.body);
     res.status(200).json(data);
