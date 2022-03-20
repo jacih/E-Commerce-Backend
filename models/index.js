c6d2838c683b4556a900af0d125b4737e89984e9
Jacqueline Hodgson <jacqueline.e.hodgson@gmail.com>
2022-03-20 10:36:46 -0400

    added models

--- models/index.js
+++ models/index.js
@@ -1,20 +1,37 @@
 // import models
-const Product = require('./Product');
 const Category = require('./Category');
-const Tag = require('./Tag');
+const Product = require('./Product');
 const ProductTag = require('./ProductTag');
+const Tag = require('./Tag');
 
-// Products belongsTo Category
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'SET NULL'
+});
+
+// Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id'
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: 'product_id'
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: 'tag_id'
+});
+
 
 module.exports = {
-  Product,
   Category,
-  Tag,
+  Product,
   ProductTag,
+  Tag,
 };
